9774a0c8ddc562654344f52448c942a57f311672
J-ustine <justine.collache@gmail.com>
2021-12-18 20:38:44 +0100

    Add the EventCard nested component to EventList component

---
+++ src/components/EventCard.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>{{ title }}</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: "Park Cleanup",
+    };
+  },
+};
+</script>
+
+<style scoped>
+h4 {
+  color: green;
+}
+</style>
