16e220f9921a93a463dc98c1d35ee219933e4598
J-ustine <justine.collache@gmail.com>
2021-12-18 18:22:46 +0100

    Update router link with my new events components

--- src/router.js
+++ src/router.js
@@ -1,27 +1,26 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import EventCreate from "../views/EventCreate.vue";
+import EventList from "../views/EventList.vue";
+import EventShow from "../views/EventShow.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: "/",
-    name: "Home",
-    component: Home,
+    name: "event-list",
+    component: EventList,
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+    path: "/create",
+    name: "event-create",
+    component: EventCreate,
   },
   {
-    path: "/about-us",
-    redirect: { name: "About" },
+    path: "/show",
+    name: "event-show",
+    component: EventShow,
   },
 ];
 
